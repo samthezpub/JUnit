2b1778714ac98bf35f8eb0ad65a00f1cc29fe09c
Владимир <lil.rhymes9@gmail.com>
2023-09-20 17:00:08 +0300

    Circle Tests

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -1,20 +1,40 @@
 package org.example;
 
+import org.example.Figures.Circle;
 import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * Unit test for simple App.
  */
-public class AppTest
-{
+public class AppTest {
 
     @Test
-    public void testApp()
-    {
+    public void testApp() {
         String helloWorld = "Hello World!";
-        assertEquals( "Hello World!", helloWorld);
+        assertEquals("Hello World!", helloWorld);
     }
 
+
+    @Test
+    public void Circle_calculateSquare_MustReturnSquareTypoDoubleWhenCalled() {
+        Circle circle = new Circle(10);
+        circle.calculateSquare();
+
+        double expected = Math.PI * Math.pow(10, 2);
+
+        assertEquals(expected, circle.getSquare());
+    }
+
+    @Test
+    public void Circle_calculatePerimeter_MustReturnSquareTypoDoubleWhenCalled() {
+        Circle circle = new Circle(10);
+        circle.calculatePerimeter();
+
+        double expected = 2 * Math.PI * 10;
+
+        assertEquals(expected, circle.getPerimeter());
+    }
 
 }
