05d0cb8515e065c287ab783eaeaf3e8c45602db6
Владимир <lil.rhymes9@gmail.com>
2023-09-20 16:26:35 +0300

    Init commit

---
+++ src/test/java/org/example/AppTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest
+{
+
+    @Test
+    public void testApp()
+    {
+        String helloWorld = "Hello World!";
+        assertEquals( "Hello World!", helloWorld);
+    }
+
+
+}
